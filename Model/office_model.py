c6dac9db49dbc7b5ddc49ed14219c7a2d504a678
dsteenge <dicksteenge@outlook.com>
2017-11-05 14:12:23 +0100

    Office hours instellen afgemaakt en netjes gemaakt

--- Model/office_model.py
+++ Model/office_model.py
@@ -7,9 +7,13 @@ from View_Widgets import settings_officehours as office
 def onEnterScript():
     office.office1.setTooltip("Rollucks will close outside\nthese hours")
 
+
 def onLeaveScript():
     office.office1.setTooltip("")
 
+
+closehour = 23
+closeminute = 0
 openhour = 8
 openminute = 0
 
@@ -19,34 +23,69 @@ def increaseOpenTimeScript():
     if openhour == 23 and openminute == 50:
         openhour = 0
         openminute = 0
-        time = "{}0:{}0".format(openhour,openminute)
+        time = "{}:{}0".format(openhour,openminute)
     elif openminute == 50:
         openminute = 0
         openhour+=1
-        time = "{}:{}0".format(openhour,openminute)
+        time = "{}:{}0".format(openhour, openminute)
     else:
         openminute+= 10
-        time = "{}:{}".format(openhour,openminute)
+        time = "{}:{}".format(openhour, openminute)
     office.office1.officeOpenValue.config(text=time)
+
 
 def decreaseOpenTimeScript():
     global openhour
     global openminute
-    if openminute == 10:
-        openminute = 0
-        time = "{}:{}0".format(openhour,openminute)
-    elif openminute <= 0:
-        openhour-=1
+    if openminute >= 10:
+        openminute -= 10
+        if openminute == 0:
+            time = "{}:{}0".format(openhour, openminute)
+        else:
+            time = "{}:{}".format(openhour,openminute)
+    elif openhour > 0 and openminute == 0:
         openminute = 50
+        openhour -= 1
         time = "{}:{}".format(openhour,openminute)
-    else:
-        openminute-= 10
-        time = "{}:{}".format(openhour,openminute)
-
+    elif openhour == 0 and openminute == 0:
+        openhour = 23
+        openminute = 50
+        time = "{}:{}".format(openhour, openminute)
     office.office1.officeOpenValue.config(text=time)
 
+
 def increaseCloseTimeScript():
-    pass
+    global closehour
+    global closeminute
+    if closehour == 23 and closeminute == 50:
+        closehour = 0
+        closeminute = 0
+        time = "{}:{}0".format(closehour,closeminute)
+    elif closeminute == 50:
+        closeminute = 0
+        closehour+=1
+        time = "{}:{}0".format(closehour,closeminute)
+    else:
+        closeminute+= 10
+        time = "{}:{}".format(closehour,closeminute)
+    office.office1.officeCloseValue.config(text=time)
+
 
 def decreaseCloseTimeScript():
-    pass
+    global closehour
+    global closeminute
+    if closeminute >= 10:
+        closeminute -= 10
+        if closeminute == 00:
+            time = "{}:{}0".format(closehour,closeminute)
+        else:
+            time = "{}:{}".format(closehour, closeminute)
+    elif closehour > 0 and closeminute == 0:
+        closehour-=1
+        closeminute = 50
+        time = "{}:{}".format(closehour,closeminute)
+    elif closehour == 0 and closeminute == 0:
+        closehour = 23
+        closeminute = 50
+        time = "{}:{}".format(closehour, closeminute)
+    office.office1.officeCloseValue.config(text=time)
