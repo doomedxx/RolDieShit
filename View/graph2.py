da0fd98c6c2550a53a7a337749fa11f35e95124c
doomedxx <wesley.dw@live.nl>
2017-11-08 22:11:03 +0100

    Mini-fix#2

--- View/graph2.py
+++ View/graph2.py
@@ -16,7 +16,7 @@ class graphwidget2(object):
     graphWidget2.place(height=220, width=300, x=25, y=180)
     graphWidget2.config(bg=value.widgetBackground,borderwidth= value.borderWidth, relief=value.relief)
 
-    f = Figure(figsize=(5,5), dpi=80, facecolor='#707a75')
+    f = Figure(figsize=(5,5), dpi=80, facecolor='#9a9ca0')
     a = f.add_subplot(111)
     a.plot([5,10,15,20,25,30],[13,13,14,15,14,14])
 
@@ -24,8 +24,8 @@ class graphwidget2(object):
     canvas.show()
     canvas.get_tk_widget().pack(side=mainframe.TOP, expand=True)
 
-    graphlabel2 = Label(graphWidget2, text="|   TEMPERATURES")
-    graphlabel2.config(font=(value.titlefont), bg="#707a75", fg=value.titleColor)
+    graphlabel2 = Label(graphWidget2, text="TEMPERATURES")
+    graphlabel2.config(font=(value.titlefont), bg="#9a9ca0", fg=value.titleColor)
     graphlabel2.pack()
     graphlabel2.place(x=5, y=-2)
 
--- View/graph3.py
+++ View/graph3.py
@@ -1,8 +1,10 @@
 import matplotlib
 matplotlib.use("TkAgg")
-
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
 from matplotlib.figure import Figure
+import numpy as np
+
+
 
 from tkinter import *
 
@@ -13,28 +15,31 @@ import View.widgetValues as value
 class graphwidget3(object):
     graphWidget3 = Frame(mainframe.root, relief=SUNKEN)
 
-    f = Figure(figsize=(7,5), dpi=80, facecolor='#707a75')
+    f = Figure(figsize=(10,5), dpi=100, facecolor='#9a9ca0')
+    f.tight_layout()
     a = f.add_subplot(111)
-    a.plot([10,20,30,40,50,60,70,80],[2,3,4,1,4,6,4,2])
+    x = np.array(range(1,13))
+    y = np.random.randint(80, size=x.shape)
+    a.plot(x,y)
 
     canvas = FigureCanvasTkAgg(f, graphWidget3)
-    canvas.show()
-    canvas.get_tk_widget().pack(side=mainframe.TOP,expand=True)
+    canvas.get_tk_widget().grid(row=20,column=6,sticky=W)
+    canvas.get_tk_widget().pack(expand=True)
+    canvas.get_tk_widget().config(height=220)
 
-    graphWidget3.pack(side=LEFT)
+    graphWidget3.pack(side=RIGHT)
     graphWidget3.place(height=220, width=525, x=330, y=180)
     graphWidget3.config(bg=value.widgetBackground,borderwidth= value.borderWidth, relief=value.relief)
-    graphlabel31 = Label(graphWidget3, text="|  LIGHT LEVELS")
-    graphlabel31.config(font=(value.titlefont), bg="#707a75", fg="white")
+    graphlabel31 = Label(graphWidget3, text="LIGHT LEVELS")
+    graphlabel31.config(font=(value.titlefont), bg="#9a9ca0", fg="white")
     graphlabel31.pack()
     graphlabel31.place(x=10, y=-2)
 
     toolbar = NavigationToolbar2TkAgg(canvas, graphWidget3)
     toolbar.update()
-    canvas._tkcanvas.pack(side=mainframe.RIGHT,expand=True)
+    canvas._tkcanvas.pack(side=mainframe.TOP,expand=True)
 
 def replace():
-    g3.graphWidget3.place(height=220, width=525, x=330, y=180)
     g3.graphWidget3.place(height=220, width=525, x=330, y=180)
 
 g3 = graphwidget3()
